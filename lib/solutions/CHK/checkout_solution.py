1e1c04cf753c838e4f6ad8547f33a6e1a5ba4fd3
sbx_user1051 <sbx_user1051@169.254.176.21>
2022-05-23 13:56:30 +0000

    Mon May 23 13:56:30 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -18,12 +18,25 @@ def checkout(skus):
     countV = 0
     total = 0
     
+    count20 = 0
+    countX = 0
+    countZ = 0
+    groupbuy = set(['S','T','X','Y','Z'])
+    
     cost = {'C':20,'D':15,'G':20,'I':35,
-            'J':60,'L':90,'O':10,'S':30,
-            'T':20,'W':20,'X':90,'Y':10,'Z':50}
+            'J':60,'L':90,'O':10,'S':20,
+            'T':20,'W':20,'X':17,'Y':20,'Z':21}
     
     for x in skus:
-        if x in cost:
+        if x in groupbuy:
+            if x == 'X':
+                countX += 1
+            elif x == 'Z':
+                countZ += 1
+            else:
+                count20 += 1
+            
+        elif x in cost:
             total += cost[x]
         elif x == 'A':
             countA += 1
@@ -97,10 +110,10 @@ def checkout(skus):
         
     # offer for K
     if countK > 1:
-        total += ((countK//2)*150) +  ((countK%2)*80)
+        total += ((countK//2)*120) +  ((countK%2)*70)
     
     else:
-        total += 80*countK
+        total += 70*countK
         
     # offer for N and M 
     if countN > 2:
@@ -144,5 +157,38 @@ def checkout(skus):
     
     else:
         total += 50*countV
+        
+    # groupbuy
+    groupbuy = countX + countZ + count20
+    
+    if groupbuy > 2:
+        total += (groupbuy//3)*45
+        groupbuy = groupbuy%3
+        
+    else:
+        groupbuy = 0
+        total += countX*17+count20*20+countZ*21
+    
+    if groupbuy > 0:
+        
+        if countX >= groupbuy:
+            total += 17*groupbuy
+        
+        else:
+            total += countX*17
+            groupbuy = max(0,groupbuy-countX)
+            
+            if count20 >= groupbuy:
+                total += 20*groupbuy
+            
+            else:
+                total += count20*20
+                groupbuy = max(0,groupbuy-count20)
+                
+                total += groupbuy*21
+                    
+                    
+
     
     return total
+
